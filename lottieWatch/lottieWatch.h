9ec8b96b65de6a99502bcfbb7d4f3d5a5dea169f
Andrew Simpson <andrewsimpson@andrews-macbook-pro-2.local>
2018-12-03 18:53:50 -0600

    Initial Commit

---
+++ lottieWatch/lottieWatch.h
@@ -0,0 +1,19 @@
+//
+//  lottieWatch.h
+//  lottieWatch
+//
+//  Created by Andrew Simpson on 12/3/18.
+//  Copyright © 2018 Andrew Simpson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for lottieWatch.
+FOUNDATION_EXPORT double lottieWatchVersionNumber;
+
+//! Project version string for lottieWatch.
+FOUNDATION_EXPORT const unsigned char lottieWatchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <lottieWatch/PublicHeader.h>
+
+
